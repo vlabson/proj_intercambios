99e3f125af14f0e29c7f4c2a8471539e34aaf3e1
vlabson silva viana <vlabson@opovodigital.com>
2022-09-15 21:08:59 -0300

    inserindo identação

--- wp-content/themes/alura-intercambios/footer.php
+++ wp-content/themes/alura-intercambios/footer.php
@@ -1,11 +1,13 @@
- <footer>
-    <p class="container-alura">&copy; <?= date(format: "Y") ?> - Todos os dereitos reservados a Alura Intercambios</p>
- </footer>
+   <footer>
+      <p class="container-alura">&copy; <?= date(format: "Y") ?> - Todos os dereitos reservados a Alura Intercambios</p>
+   </footer>
 
 </body>
+
 <?php 
 /* aqui chamamos uma função nativa do WP que são ganchos destinados ao cabeçalho e ao rodapé de um tema do WP 
 OBS: ele unido ao wp_head em suas devidas pastas fazem exibir o menu de controle do WP */
 Wp_footer(); 
 ?>
+
 </html>
